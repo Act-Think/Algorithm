abf436a034afab039f3349ec75f629d67ec3b908
act_think <angel4yj@daum.net>
2020-07-01 02:32:38 +0900

    a

--- baekjoon11-15/05_other.js
+++ baekjoon11-15/05_other.js
@@ -4,7 +4,7 @@
 회의의 시작시간과 끝나는 시간이 같을 수도 있다.
 
 첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)
-시작 시간과 끝나는 시간은 2^31-1보다 작거나 같은 자연수 또는 0이다.
+시작 시간과 끝나는 시간은 231-1보다 작거나 같은 자연수 또는 0이다.
 */
 'use strict'
 // var fs = require('fs');
@@ -13,10 +13,26 @@
 var input = "11\n1 4\n3 5\n0 6\n5 7\n3 8\n5 9\n6 10\n8 11\n8 12\n2 13\n12 14"
 var inputSplit = input.split('\n');
 var n = parseInt(inputSplit[0]);
-
-var arr=[];
-for (var i=1; i<=n; i++) {
+var arr = [];
+for (var i = 1; i<=n; i++) {
     var [start, end] = input[i].split(' ');
     arr.push([+start, +end]);
 }
-console.log(arr);
+
+arr.sort((a, b) => {
+    if(a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
+});
+
+var answer = 0;
+var time = 0;
+
+arr.forEach(e => {
+    if(time <= e[0]) {
+        answer += 1;
+        time = e[1];
+
+    }
+});
+
+console.log(answer);
--- baekjoon11-15/07.js
+++ baekjoon11-15/07.js
@@ -1,14 +1,16 @@
 // 4	1541	잃어버린 괄호
 /*
-
-
-
+식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있고,
+가장 처음과 마지막 문자는 숫자이다.
+연속해서 두 개 이상의 연산자가 나타나지 않고,
+5자리보다 많이 연속되는 숫자는 없다.
+수는 0으로 시작할 수 있다.
 */
 
 'use strict'
 // var fs = require('fs');
 // var input = fs.readFileSync('/dev/stdin').toString();
 
-var input = "5\n3 1 4 3 2"
-var inputN = input.split('\n');
-var n = parseInt(inputN[0]);
+var input = "55-50+40"
+input.replace(/-[0-9]+(?=-)/,/-\([0-9]+\)/);
+console.log(input);
